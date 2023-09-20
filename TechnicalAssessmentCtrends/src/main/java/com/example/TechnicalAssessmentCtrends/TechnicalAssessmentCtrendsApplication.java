20cbc805f9b2443594056a9187c961e5a29ffff1
TKGL\imran <s.arifinoriginal@gmail.com>
2023-09-20 09:43:14 +0600

    start

---
+++ TechnicalAssessmentCtrends/src/main/java/com/example/TechnicalAssessmentCtrends/TechnicalAssessmentCtrendsApplication.java
@@ -0,0 +1,13 @@
+package com.example.TechnicalAssessmentCtrends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalAssessmentCtrendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalAssessmentCtrendsApplication.class, args);
+	}
+
+}
---
+++ TechnicalAssessmentCtrends/src/test/java/com/example/TechnicalAssessmentCtrends/TechnicalAssessmentCtrendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TechnicalAssessmentCtrends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalAssessmentCtrendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
